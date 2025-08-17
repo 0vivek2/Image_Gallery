88eb4a1332ae9da806c191265992bca9a82fa90d
VIVEK <vkvillain2000@gmail.com>
2025-08-17 12:09:02 +0530

    Second Change

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Image_Gallery/',
+  
   plugins: [react()],
+  base: '/Image_Gallery/',
 })
